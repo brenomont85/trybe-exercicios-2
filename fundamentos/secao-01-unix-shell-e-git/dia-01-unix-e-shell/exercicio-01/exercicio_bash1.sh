597cf679311d569f4526399828d152085933c06d
Breno Monteiro <brenomont85@gmail.com>
2026-08-13 17:21:45 -0300

    creates script file for exercicio_bash1, solves first requirement, runs script.

---
+++ fundamentos/secao-01-unix-shell-e-git/dia-01-unix-e-shell/exercicio-01/exercicio_bash1.sh
@@ -0,0 +1,6 @@
+#Exercícios Bash - 1
+#1.Utilizando o terminal, aplique o comando de criação de diretórios que você aprendeu: crie um diretório chamado unix_tests e navegue até ele.
+mkdir -p unix_tests
+cd unix_tests
+
+
