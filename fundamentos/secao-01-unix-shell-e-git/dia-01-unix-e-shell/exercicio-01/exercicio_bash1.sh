a1ed46731847f99cc1ce4f301d6a41ee18c974a1
Breno Monteiro <brenomont85@gmail.com>
2026-08-13 17:24:12 -0300

    solves second requirement, runs script.

--- fundamentos/secao-01-unix-shell-e-git/dia-01-unix-e-shell/exercicio-01/exercicio_bash1.sh
+++ fundamentos/secao-01-unix-shell-e-git/dia-01-unix-e-shell/exercicio-01/exercicio_bash1.sh
@@ -3,4 +3,11 @@
 mkdir -p unix_tests
 cd unix_tests
 
+#2.Crie um arquivo de texto pelo terminal com o nome skills2.txt e adicione os valores Internet, Unix e Bash, cada um em sua linha.
+cat << EOF > skills2.txt
+Internet
+Unix
+Bash
+EOF
+
 
