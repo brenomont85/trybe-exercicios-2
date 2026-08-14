c1b92d6df332630ca5c6d7e9b9efeed972b0196e
Breno Monteiro <brenomont85@gmail.com>
2026-08-14 14:59:27 -0300

    solves requirement number 4, 5, 6  and 7. runs script

--- fundamentos/secao-01-unix-shell-e-git/dia-01-unix-e-shell/exercicio-02/exercicio_bash2.sh
+++ fundamentos/secao-01-unix-shell-e-git/dia-01-unix-e-shell/exercicio-02/exercicio_bash2.sh
@@ -13,3 +13,35 @@ curl -o countries.txt "https://gist.githubusercontent.com/kalinchernev/486393efc
 #3.Mostre todo o conteúdo do arquivo countries.txt na tela.
 cat countries.txt
 
+#Instrução para uso do comando less
+
+clear
+
+echo -e "\n Embreve entraremos em modo de paginação, use a barra de espaço para ir para a próxima página até o fim do arquivo ou pressione q para sair\n"
+
+sleep 8
+
+clear
+
+#4.Mostre o conteúdo de countries.txt, página por página, até encontrar a Zambia.
+less -E countries.txt
+
+sleep 2
+
+#Novo modo de paginação
+
+echo "Modo de paginação com busca: aperte 'q' para sair"
+sleep 5
+clear
+
+#5.Mostre novamente o conteúdo de countries.txt página por página, mas agora utilize um comando para buscar por Zambia.
+less +/Zambia countries.txt
+
+sleep 2
+
+#6.Busque por Brazil no countries.txt.
+grep Brazil countries.txt
+
+#7.Busque novamente por brazil, mas agora utilize o lower case e não considere letras maiúsculas ou minúsculas.
+grep -i brazil countries.txt
+
